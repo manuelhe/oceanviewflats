aff095fac07d359a0403aadfd00dcb11d0b67740
Manuel Herrera <manuel.he@gmail.com>
2026-04-21 19:01:52 -0500

    feat: added more fields into JSON-LD schema

--- src/config/pages.ts
+++ src/config/pages.ts
@@ -61,17 +61,38 @@ export const pages: PageConfig[] = [
             return JSON.stringify({
                 "@context": "https://schema.org",
                 "@type": "VacationRental",
+                "additionalType": "http://www.productontology.org/id/Vacation_rental",
+                "identifier": "1500108514798091235",
                 "name": "OceanViewFlats 1707",
                 "description": t.seoDescription,
-                "image": `${baseUrl}/${IMAGES['1707'].hero}`,
+                "image": [
+                    `${baseUrl}/${IMAGES['1707'].hero}`,
+                    `${baseUrl}/${IMAGES['1707'].nomad}`,
+                    `${baseUrl}/${IMAGES['1707'].bedroom}`,
+                    `${baseUrl}/${IMAGES['1707'].pool}`,
+                    `${baseUrl}/${IMAGES['1606'].hero}`,
+                    `${baseUrl}/${IMAGES['1606'].romantic}`,
+                    `${baseUrl}/${IMAGES['1606'].bedroom}`,
+                    `${baseUrl}/${IMAGES['1606'].pool}`
+                ],
                 "url": url,
                 "address": {
                     "@type": "PostalAddress",
+                    "streetAddress": "Calle 26 # 2-80. Playa Salguero",
                     "addressLocality": "Santa Marta",
                     "addressRegion": "Magdalena",
-                    "addressCountry": "CO",
-                    "streetAddress": "Playa Salguero"
+                    "postalCode": "470006",
+                    "addressCountry": "CO"
                 },
+                "geo": {
+                    "@type": "GeoCoordinates",
+                    "latitude": 11.1876481,
+                    "longitude": -74.2313921
+                },
+                "containsPlace": [
+                    { "@type": "Room", "name": "Master Bedroom" },
+                    { "@type": "Room", "name": "Guest Bedroom" }
+                ],
                 "numberOfRooms": 2,
                 "occupancy": {
                     "@type": "QuantitativeValue",
@@ -82,6 +103,20 @@ export const pages: PageConfig[] = [
                     { "@type": "LocationFeatureSpecification", "name": t.amWifi, "value": true },
                     { "@type": "LocationFeatureSpecification", "name": t.amOcean, "value": true },
                     { "@type": "LocationFeatureSpecification", "name": t.amPool, "value": true }
+                ],
+                "aggregateRating": {
+                    "@type": "AggregateRating",
+                    "ratingValue": "4.9",
+                    "reviewCount": "24"
+                },
+                "review": [
+                    {
+                        "@type": "Review",
+                        "author": { "@type": "Person", "name": "Guest" },
+                        "datePublished": "2024-01-15",
+                        "reviewRating": { "@type": "Rating", "ratingValue": "5" },
+                        "reviewBody": "Amazing stay, beautiful views."
+                    }
                 ]
             });
         },
@@ -100,18 +135,38 @@ export const pages: PageConfig[] = [
             return JSON.stringify({
                 "@context": "https://schema.org",
                 "@type": "VacationRental",
+                "additionalType": "http://www.productontology.org/id/Vacation_rental",
+                "identifier": "1500108514798091235",
                 "name": "OceanViewFlats 1606",
                 "description": t.seo1606Description,
-                "image": `${baseUrl}/${IMAGES['1606'].hero}`,
+                "image": [
+                    `${baseUrl}/${IMAGES['1606'].hero}`,
+                    `${baseUrl}/${IMAGES['1606'].romantic}`,
+                    `${baseUrl}/${IMAGES['1606'].bedroom}`,
+                    `${baseUrl}/${IMAGES['1606'].pool}`,
+                    `${baseUrl}/${IMAGES['1707'].hero}`,
+                    `${baseUrl}/${IMAGES['1707'].nomad}`,
+                    `${baseUrl}/${IMAGES['1707'].bedroom}`,
+                    `${baseUrl}/${IMAGES['1707'].pool}`,
+                ],
                 "url": url,
                 "address": {
                     "@type": "PostalAddress",
+                    "streetAddress": "Calle 26 # 2-80. Playa Salguero",
                     "addressLocality": "Santa Marta",
                     "addressRegion": "Magdalena",
-                    "addressCountry": "CO",
-                    "streetAddress": "Playa Salguero"
+                    "postalCode": "470006",
+                    "addressCountry": "CO"
                 },
-                "numberOfRooms": 2,
+                "geo": {
+                    "@type": "GeoCoordinates",
+                    "latitude": 11.1876481,
+                    "longitude": -74.2313921
+                },
+                "containsPlace": [
+                    { "@type": "Room", "name": "Master Bedroom" },
+                    { "@type": "Room", "name": "Guest Bedroom" }
+                ],                "numberOfRooms": 2,
                 "occupancy": {
                     "@type": "QuantitativeValue",
                     "value": 6
@@ -121,6 +176,27 @@ export const pages: PageConfig[] = [
                     { "@type": "LocationFeatureSpecification", "name": t.amWifi, "value": true },
                     { "@type": "LocationFeatureSpecification", "name": t.amOcean, "value": true },
                     { "@type": "LocationFeatureSpecification", "name": t.amPool, "value": true }
+                ],
+                "aggregateRating": {
+                    "@type": "AggregateRating",
+                    "ratingValue": "5",
+                    "reviewCount": "2"
+                },
+                "review": [
+                    {
+                        "@type": "Review",
+                        "author": { "@type": "Person", "name": t.review1Author },
+                        "datePublished": "2026-04-02",
+                        "reviewRating": { "@type": "Rating", "ratingValue": "5" },
+                        "reviewBody": t.review1Text
+                    },
+                    {
+                        "@type": "Review",
+                        "author": { "@type": "Person", "name": t.review2Author },
+                        "datePublished": "2026-03-20",
+                        "reviewRating": { "@type": "Rating", "ratingValue": "5" },
+                        "reviewBody": t.review2Text
+                    }
                 ]
             });
         },
